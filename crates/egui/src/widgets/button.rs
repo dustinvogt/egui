2e85d1e4a2f284611b02f1db3eada75667669efc
Dustin Vogt <djvogt@gmail.com>
2026-05-03 14:04:39 -0400

    test fix 1

--- crates/egui/src/widgets/button.rs
+++ crates/egui/src/widgets/button.rs
@@ -353,7 +353,11 @@ impl<'a> Button<'a> {
         layout = if has_frame_margin && (state != WidgetState::Inactive || frame_when_inactive) {
             layout.frame(frame)
         } else {
-            layout.frame(Frame::new().inner_margin(frame.inner_margin))
+            let mut temp_frame = Frame::new().inner_margin(frame.inner_margin);
+            if let Some(stroke) = stroke {
+                temp_frame = temp_frame.stroke(Stroke::new(stroke.width, Color32::TRANSPARENT));
+            }
+            layout.frame(temp_frame)
         };
 
         let mut prepared = layout.min_size(min_size).allocate(ui);
